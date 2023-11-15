061e9e097535711dd5272dea6e02c539401b41ef
Marcus-Silva <92951909+SilvaMarcus@users.noreply.github.com>
2023-11-15 12:46:20 -0300

    .
    .

--- agenda.rb
+++ agenda.rb
@@ -1,33 +1,73 @@
-@agenda = [{nome: "Marcus",  telefone: "11953594786"},{nome: "Vinicius",  telefone: "11994452551"}]
+@agenda = [{nome: "Marcus", telefone: "11994452551"},{nome: "Vinicius", telefone: "11953594786"}]
 
-def all_contacts
-  puts "............................"
- @agenda.each do |contato|
- puts "Nome: #{contato[:nome]} Telefone: #{contato[:telefone]}"
+def listarContatos
+  @agenda.each do |event|
+  puts "Nome: #{event[:nome]} Telefone: #{event[:telefone]}"
   end
-  puts "............................"
 end
 
-def add_contact
-  print "Nome: "
+def adiconar
+  print "Qual o nome ?"
   nome = gets.chomp
-  print "Telefone: "
+  print "Qual o Telefone ?"
   telefone = gets.chomp
   @agenda << {nome: nome, telefone: telefone}
 end
 
-loop do
-  puts "SELECIONE UMA OPÇÃO:\n \n1. Contatos\n2. Adicionar Contatos\n3. Ver Contato\n4. Editar Contato\n5. Excluir Contato \n0. Sair"
-  opcao = gets.chomp.to_i
+def verContato
 
-  case opcao
-  when 0
-    puts "Ate mais! "
-    break
-  when 1
-    all_contacts
-  when 2
-    add_contact
+  print "Qual Contato voce quer buscar ? "
+  nomeContato = gets.chomp
+
+  found = false
+
+  @agenda.each do |element|
+    if element[:nome].downcase.include?(nomeContato.downcase)
+      puts "__________"
+      puts "Nome: #{element[:nome]} Telefone: #{element[:telefone]}"
+      puts "__________"
+      found = true
+    end
   end
+  puts "Não encontrado" unless found
+end
 
+def editarContato
+  print "Qual nome deseja editar ?"
+  nome = gets.chomp
+
+  @agenda.each do |element|
+    if element[:nome].downcase.include?(nome.downcase)
+        print "Qual nome de seja atribuir ?"
+      novo_nome = gets.chomp
+
+        element[:nome] = novo_nome
+        puts "Nome: #{element[:nome]} Telefone: #{element[:telefone]}"
+    end
+  end
+end 
+
+
+loop do
+puts "SELECIONE UMA OPÇÃO:\n1. Contatos\n2. Adicionar Contatos\n3. Ver Contato\n4. Editar Contato\n5. Excluir Contato\n0. Sair"
+
+option = gets.chomp.to_i
+
+case option
+when 0
+ puts  "Ate logo!"
+ break
+
+when 1
+  listarContatos
+  break
+
+when 2
+  adiconar
+when 3
+  verContato
+  break
+when 4
+  editarContato
+end
 end
